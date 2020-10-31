945e700df4265627598de0626cecb9d390702aa0
Rupin Dhanoa <rdhanoa@cisco.com>
2020-10-31 14:57:22 -0700

    started site class

---
+++ ssg.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import typer
+from ssg.site import Site
+
+
+def main(source="content", dest="dist"):
+    '''
+
+    :param source:
+    :param dest:
+    :return:
+    '''
+    config = {"source": source, "dest": dest}
