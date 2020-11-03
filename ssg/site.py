81980e6840926329bda9b1c7a1717410f643b316
Rupin Dhanoa <rdhanoa@cisco.com>
2020-11-03 07:16:54 -0800

    started site class

--- ssg/site.py
+++ ssg/site.py
@@ -54,6 +54,6 @@ class Site():
         """
         parser = self.load_parser(path.suffix)
         if parser is not None:
-            parser.parse(path, source, dest)
+            parser.parse(path, self.source, self.dest)
         else:
             print ("Not Implemented")
