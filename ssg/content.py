0b57fe94f3f4e3300a25548cec2e5ee2ff81798d
Rupin Dhanoa <rdhanoa@cisco.com>
2020-11-05 09:20:37 -0800

    started site class

--- ssg/content.py
+++ ssg/content.py
@@ -13,3 +13,14 @@ class Content(Mapping):
         __delimiter = r"^(?:-|\+){3}\s*$"
         __regex = re.compile(__delimiter, re.MULTILINE)
 
+    def load(self, cls, string):
+        """
+        load method for Content
+        :param cls:
+        :param string:
+        :return:
+        """
+
+        _, fm, content = self.__regex.split(string, 2)
+        load(fm, Loader=FullLoader)
+        return cls(metadata, content)
