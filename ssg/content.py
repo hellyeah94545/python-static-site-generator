ee5e0df648b3e71f1528543100d1b50964a58010
Rupin Dhanoa <rdhanoa@cisco.com>
2020-11-13 09:24:07 -0800

    started site class

--- ssg/content.py
+++ ssg/content.py
@@ -33,6 +33,10 @@ class Content(Mapping):
     def body(self):
         return self.data["content"]
 
+    # @property
+    # def type(self):
+    #     return self.data["type"] if "type" in self.data else None
+
     @property
     def type(self):
         return self.data["type"] if "type" in self.data else None
