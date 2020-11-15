785a433fce8465725b238daa513996fb55794c00
Rupin Dhanoa <rdhanoa@cisco.com>
2020-11-15 09:43:56 -0800

    started site class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -105,4 +105,4 @@ class MarkdownParser(Parser):
         html = markdown(content.body)
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format
-                         (path.name, path.content))
+                         (path.name, content))
